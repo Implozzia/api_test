46f7cc6db983dc4953d945f2ac8175e02516103b
ruslan <465274372kenny3>
2022-05-26 17:37:39 +0300

    fix for user agetn

--- user_agent.py
+++ user_agent.py
@@ -3,19 +3,15 @@ import requests
 
 
 class TestUserAgent:
-    user_agents = [(
-                   'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+    user_agents = [('Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
                    'Mobile', 'No', 'Android'),
-                   (
-                   'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
+                   ('Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
                    'Mobile', 'Chrome', 'IOS'),
                    ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
                     'Googlebot', 'Unknown', 'Unknown'),
-                   (
-                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',
+                   ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',
                    'Web', 'Chrome', 'No'),
-                   (
-                   'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+                   ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
                    'Mobile', 'No', 'iPhone')]
 
     @pytest.mark.parametrize('user_agent, expected_platform, expected_browser, expected_device', user_agents)
@@ -26,6 +22,6 @@ class TestUserAgent:
         actual_platform = response['platform']
         actual_browser = response['browser']
         actual_device = response['device']
-        assert actual_platform == expected_platform, f'We wait {expected_platform}, but actually have {actual_platform}'
-        assert actual_browser == expected_browser, f'We wait {expected_browser}, but actually have {actual_browser}'
-        assert actual_device == expected_device, f'We wait {expected_device}, but actually have {actual_device}'
+        assert actual_platform == expected_platform, f'We expected {expected_platform}, but actually have {actual_platform}'
+        assert actual_browser == expected_browser, f'We expected {expected_browser}, but actually have {actual_browser}'
+        assert actual_device == expected_device, f'We expected {expected_device}, but actually have {actual_device}'
